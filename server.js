13859b2965e68f7c72ce26e7e9fb270ebe45f837
Glitch (hello-express) <none>
2019-11-03 17:45:01 +0000

    💶😻 Checkpoint ./server.js:933874/5449

--- server.js
+++ server.js
@@ -13,15 +13,12 @@ try {
 const express = require("express");
 const app = express();
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/img", (req, res) => {
   if (fuzIsOpen && new Date() - 2 * 60 * 1000 < lastSeen) {
     return res.sendFile(__dirname + "/views/open.svg"); // https://www.flaticon.com/free-icon/open_1234189
@@ -37,7 +34,6 @@ app.get("/api", (req, res) => {
   res.send({ fuzIsOpen, lastSeen });
 });
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/status", (req, res) => {
   // http basic auth handling without 3rd-party lib https://stackoverflow.com/a/33905671
   const auth = {
@@ -73,10 +69,12 @@ const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
 
-process.on("SIGTERM", function() {
-  console.log("SIGTERM received, sending SOS to Resurrect...");
-  require("https").get(
-    "https://resurrect.glitch.me/" + process.env.PROJECT_DOMAIN + "",
-    process.exit
-  );
-});
+if (process.env.PROJECT_DOMAIN != "") {
+  process.on("SIGTERM", function() {
+    console.log("SIGTERM received, sending SOS to Resurrect...");
+    require("https").get(
+      "https://resurrect.glitch.me/" + process.env.PROJECT_DOMAIN + "",
+      process.exit
+    );
+  });
+}
